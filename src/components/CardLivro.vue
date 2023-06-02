0e0329339bff0495c0f0ae91110d570495b008d9
TinaPera <valentina.pera2007@gmail.com>
2023-06-02 15:05:30 -0300

    aulinha linda do nosso mano eduardo

---
+++ src/components/CardLivro.vue
@@ -0,0 +1,128 @@
+<script setup>
+import {adicionarAoCarrinho} from '@/_data/carrinho.js'
+const props = defineProps({
+  livro: Object
+})
+
+function formatarPreco(preco) {
+  return 'R$ ' + preco.toFixed(2).replace('.', ',')
+}
+</script>
+
+<template>
+  <div class="card-livro">
+    <div class="card-info-livro">
+      <div class="wrap-livro">
+        <img :src="props.livro.img" alt="Capa do livro" class="capa-livro" />
+      </div>
+      <p class="titulo-livro">{{ props.livro.title }}</p>
+      <p class="autor-livro">{{ props.livro.author }}</p>
+      <p class="preco-livro">{{ formatarPreco(livro.price) }}</p>
+    </div>
+    <div class="card-buttons-livros">
+      <button @click="adicionarAoCarrinho(props.livro)">Adicionar ao carrinho</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrap-carrinho .carrinho-total {
+  position: fixed;
+  bottom: 3%;
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.item-carrinho .info-livro {
+  display: flex;
+  margin-bottom: 10px;
+}
+.detalhes-livro {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+.detalhes-livro p {
+  margin: 0;
+}
+.detalhes-livro div {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.detalhes-livro input[type='number'] {
+  width: 50px;
+  text-align: center;
+  border: none;
+  border-bottom: 1px solid black;
+  background-color: transparent;
+  margin-left: 10px;
+}
+
+.detalhes-livro button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 1.5rem;
+  color: black;
+  padding: 0;
+  margin: 0;
+}
+
+.info-livro-preco {
+  margin-left: auto;
+}
+.icon-capa-livro {
+  width: 30px;
+  margin-right: 10px;
+}
+.container-geral {
+  /* display: flex;
+  justify-content: space-between; */
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+}
+
+.carrinho {
+  /* min-width: 20%; */
+}
+.listagem-livros {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card-livro {
+  margin: 5px 10px;
+  padding: 10px;
+  background-color: rgb(236, 212, 250);
+  border-radius: 10px;
+  width: 180px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.wrap-livro {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  border-radius: 10px;
+  width: 180px;
+  height: 270px;
+}
+.capa-livro {
+  width: 90%;
+  max-height: 100%;
+}
+
+.card-livro p {
+  margin: 0;
+}
+
+.card-livro .titulo-livro {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+</style>
